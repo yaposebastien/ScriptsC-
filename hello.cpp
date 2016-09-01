eefa1fe0aacfa2d04a55929bb78cdbc5238fc9c1
yaposebastien <yaposebastien@gmail.com>
2016-09-01 11:41:22 -0400

    first commit

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+// Filename hello.cpp
+// Locate /home/yankees/ScriptsC++/hello.cpp
+// This program just print out bonjour
+
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"Bonjour Sebastien."<<endl;
+}
+
