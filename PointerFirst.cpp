9652d485189b2d83abf54574e5c645b2c575129d
yaposebastien <yaposebastien@gmail.com>
2016-11-29 17:02:22 -0500

    Add some comments pointer code

--- PointerFirst.cpp
+++ PointerFirst.cpp
@@ -8,9 +8,13 @@ int numberOne;
 cout << "Please enter a number" << endl;
 cin >> numberOne;
 
+//Assign pointer to the variable numberOne.
 int* pointerNumberOne = &numberOne;
 
-cout << "The value of the number is : " << numberOne << endl;
+//Print the value of number
+cout << "The value of the number is : " << numberOne << endl; 
+
+//Print the pointer's address
 cout << "The address value of the pointer of this number is : " << pointerNumberOne << endl; 
 
 return 0;
